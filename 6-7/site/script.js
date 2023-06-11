8ce9bf4b63c947a2d8b739e350aa6dca86b56421
550tealeaves <nastasia.claire89@gmail.com>
2023-06-11 10:21:16 -0400

    added custom icons to markers

--- 6-7/site/script.js
+++ 6-7/site/script.js
@@ -59,3 +59,51 @@ function onMapClick(e) {
 } //when you click the map, it will popup with the .setContent statement
 
 map.on('click', onMapClick);
+
+
+//CREATING CUSTOM ICONS
+//NEED 2 PICS: ORIGINAL AND ITS SHADOW
+//Marker icons defined by L.icon objects - passed as an option when creating markers 
+var greenIcon = L.icon({
+    iconUrl: 'leaf-green.png', //original icon
+    shadowUrl: 'leaf-shadow.png', //shadow of the icon 
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map); //adds the icon to the map
+
+
+//DEFINING ICON CLASS - do this if making several icons w/ shared options
+
+var LeafIcon = L.Icon.extend({
+    options: {
+        shadowUrl: 'leaf-shadow.png',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -76]
+    }
+});
+
+//CREATE ALL 3 LEAF ICONS FROM THIS CLASS
+var greenIcon = new LeafIcon({ iconUrl: 'leaf-green.png' }),
+    redIcon = new LeafIcon({ iconUrl: 'leaf-red.png' }),
+    orangeIcon = new LeafIcon({ iconUrl: 'leaf-orange.png' });
+
+
+// SHORTCUT TO CREATE LEAFLET CLASS
+// L.icon = function (options) {
+// return new L.Icon(options);
+// };
+
+
+//POSITION ICONS ON THE MAP
+L.marker([51.5, -0.09], { icon: greenIcon }).addTo(map).bindPopup("I am a light green leaf.");
+L.marker([51.495, -0.083], { icon: redIcon }).addTo(map).bindPopup("I am a ruby red leaf.");
+L.marker([51.49, -0.1], { icon: orangeIcon }).addTo(map).bindPopup("I am a yellow-orange leaf.");
