8b6477b188ef2d816659fbdc90b493afc71c9d44
550tealeaves <nastasia.claire89@gmail.com>
2023-06-21 19:17:13 -0400

    finished 6-21 classnotes

--- 6-21/in-class/script.js
+++ 6-21/in-class/script.js
@@ -7,7 +7,14 @@ let map = L.map('map').setView([40.7, -73.7], 11);
 //use geojson.io - to determine lat/lon and zoom level and insert in the setView area (can start wherever you want it to ) - data up there starts at NYC
 //bigger zoom number = more zoomed out
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//http://maps.stamen.com/#terrain/12/37.7706/-122.3782
+const basemap_urls = {
+    terrain: "https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",
+    osm: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+}
+//adding different basemaps
+
+L.tileLayer(basemap_urls.terrain, { //will show the terrain layer
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
@@ -30,12 +37,37 @@ marker.bindPopup(`<b>CUNY GC</b> ${message}`); //use backticks to concatenate st
 ///https://axios-http.com/ - get the script file from here and add to head in html
 ///Live Server
 
-const subways = axios('../site/data/subways.geojson').then(resp => {
-    console.log(resp);
-    L.geoJSON(resp.data, {
-        style: { color: "#ff0000" }
-    }).addTo(map);
-}); //must use ajax to load local files and said files must be managed by a server to load it 
+
+//1ST WAY TO UPLOAD GEOJSON FILE - LOCAL FILE
+// const subways = axios('../site/data/subways.geojson').then(resp => {
+//     console.log(resp);
+//     L.geoJSON(resp.data, {
+//         style: { color: "#ff0000" }
+//     }).addTo(map);
+// }); //must use ajax to load local files and said files must be managed by a server to load it 
 //.then = promise - will wait for the axios request to finish and load the data, then run this function with the response from the file 
 
 //to use a local file downloaded on computer, file must be managed by server in order to load - (1) Use Live Server
+
+//2ND WAY TO UPLOAD GEOJSON FILE - GITHUB LINK
+//Can also upload GeoJson using Github - click raw and use the link
+axios('https://raw.githubusercontent.com/Willjfield/FOSS-for-WebGIS/main/6-21/site/data/subways.geojson').then(resp => {
+    console.log(resp.data)
+    L.geoJSON(resp.data, {
+        style: function (feature) { 
+            switch (feature.properties.rt_symbol) {
+                case 'A': case 'C': case 'E': return { color: "blue" };
+                case 'B': case 'M': case 'D': return { color: "orange" };
+                case 'N': case 'Q': case 'R': case 'W': return { color: "yellow" };
+                case '1': case '2': case '3': return { color: "red" };
+                case 'J': case 'Z': return { color: "brown" };
+                case '4': case '5': case '6': return { color: "green" };
+                case '7': return { color: "purple" };
+                case 'G': return { color: "lightgreen" };
+                case 'S': case 'L': return { color: "gray" };
+                default: return { color: "black" };
+            }
+        } //will take each feature and change it based on its properties - adds a certain color based on the letter (like real subway map) - useful for choropleth maps
+    }).addTo(map);
+})
+    
