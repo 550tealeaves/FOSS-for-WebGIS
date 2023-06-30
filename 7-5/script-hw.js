dc80901a32db618ccbd6fe2f62a95e9ba364d570
550tealeaves <nastasia.claire89@gmail.com>
2023-06-30 16:00:21 -0400

    started hw file
    uploaded GeoJson for states - need to see how to upload census occupation data

---
+++ 7-5/script-hw.js
@@ -0,0 +1,34 @@
+console.log('loaded');
+
+let map = L.map('map').setView([40.7, -73.7], 5);
+//L. - means it comes from the Leaflet library 
+//make a map object in the div with the ID map 
+//setView - sets the starting lat/long and the zoom level
+//use geojson.io - to determine lat/lon and zoom level and insert in the setView area (can start wherever you want it to ) - data up there starts at NYC
+//bigger zoom number = more zoomed in
+
+//http://maps.stamen.com/#terrain/12/37.7706/-122.3782
+const basemap_urls = {
+    terrain: "https://stamen-tiles.a.ssl.fastly.net/terrain/{z}/{x}/{y}.jpg",
+    osm: "https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+}
+//adding different basemaps
+
+L.tileLayer(basemap_urls.terrain, { //will show the terrain layer
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+//L.tileLayer - comes directly from Leaflet library
+//wants a URL from were to get the tiles 
+
+const allStates = axios('usState.json').then(resp => { //brings in the map data 
+    console.log(resp); //see response in console log
+
+    L.geoJSON(resp.data, { //access the response.data and style it 
+        style: { 
+            opacity: 0.85, //higher the number the more opaque it is
+            color: "magenta", 
+            weight: 2 } //higher the number, thicker the lines are 
+    }).addTo(map).bringToFront(); //brought the map to the front so it sits on top
+
+}) //map shows up 
