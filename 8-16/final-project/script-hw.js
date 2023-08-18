d9f9819486a90d93e1e55a7027803f69ee09a776
550tealeaves <nastasia.claire89@gmail.com>
2023-08-18 12:04:48 -0400

    removed commented out code

--- 8-16/final-project/script-hw.js
+++ 8-16/final-project/script-hw.js
@@ -416,9 +416,3 @@ var popup = L.popup();
 
 map.attributionControl.addAttribution('Population data &copy; <a href="http://census.gov/">US Census Bureau</a>');
  
-
-
-
-// const profFields = { 'prof': { 'male': 'Male_ProfessionalandRelated', 'female': 'Fem_ProfessionalandRelated', 'majority': 'M_F_ProfessionalandRelated', }, 'mbfo': {    //  (...fill this in like prof)    },    //  (...fill this in like above)}let userSelection = '';
-
-// function selectEventHandler(e){    userSelection = e.value}document.getElementById('select-jobs').addEventListener('change', selectEventHandler);function getColorMFProf(d) {    const fields = profFields[userSelection];    const maleValue = d.properties[fields.male];    const femaleValue = d.properties[fields.female];    const majorityValue = d.properties[fields.majority];    return majorityValue == 'F' ? '#fdae6b' :    majorityValue == 'M' ? '#542788' :            '#ffffff';}
